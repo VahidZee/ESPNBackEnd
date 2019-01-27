148cd9f6454740ab6c5b5d96faeed6fe7894bbc6
vahidzee <vahid98zee@gmail.com>
2019-01-27 16:04:32 +0330

    User Model was updated

---
+++ Espn/methods.py
@@ -0,0 +1,61 @@
+from django.http import JsonResponse
+from django.contrib.auth.hashers import check_password
+import Espn.models as EspnModels
+import secrets as python_secrets
+
+
+def create_new_access_token(profile: EspnModels.Profile) -> str:
+    profile.token = python_secrets.token_urlsafe(nbytes=256)
+    profile.save()
+    return profile.token
+
+
+def user_password_was_incorrect() -> JsonResponse:
+    return JsonResponse(data={
+        'ok': False,
+        'description': "Password was Incorrect",
+    })
+
+
+def token_was_not_found() -> JsonResponse:
+    return JsonResponse(data={
+        'ok': False,
+        'description': "Incorrect Access Token",
+    })
+
+
+def user_profile_not_found() -> JsonResponse:
+    return JsonResponse(data={
+        'ok': False,
+        'description': "User Profile Was Not Found",
+    })
+
+
+def api_method_not_found() -> JsonResponse:
+    return JsonResponse(data={
+        'ok': False,
+        'description': "Method Not Found - For Security Reasons API Methods are only available through POST method",
+    })
+
+
+def get_user(token) -> EspnModels.Profile:
+    try:
+        profile = EspnModels.Profile.objects.get(token__exact=token)
+    except Exception:
+        raise Exception
+    return profile
+
+
+def login(request) -> JsonResponse:
+    try:
+        profile = EspnModels.Profile.objects.get(user__username__exact=request.POST['username'])
+        if not check_password(password=request.POST['password'], encoded=profile.user.password):
+            return user_password_was_incorrect()
+    except Exception:
+        return user_profile_not_found()
+    token = create_new_access_token(profile)
+    return JsonResponse(data={
+        'ok': True,
+        'token': token
+    })
+
