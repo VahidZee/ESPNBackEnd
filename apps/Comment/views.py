c45a2060f1387025839165d1b4ab359b3088b4d6
vahidzee <vahid98zee@gmail.com>
2019-02-01 19:06:58 +0330

    Comment: Done

--- apps/Comment/views.py
+++ apps/Comment/views.py
@@ -1,6 +1,6 @@
 # General Imports
 from django.http import JsonResponse
-
+import json
 # Models Imports
 from . import models as comment_models
 from apps.Espn import models as espn_models
@@ -9,7 +9,7 @@ from apps.Espn import models as espn_models
 from apps.Espn.methods import find_profile_decorator, find_profile_if_exists_decorator
 
 # Constant Values
-POSTS_PER_PAGE = 1
+POSTS_PER_PAGE = 3
 
 
 @find_profile_if_exists_decorator
@@ -85,5 +85,89 @@ def like_comment(request, comment_id: int, profile: espn_models.Profile):
             data={
                 'ok': False,
                 'description': 'Unable to like comment'
+            }
+        )
+
+
+@find_profile_decorator
+def unlike_comment(request, comment_id: int, profile: espn_models.Profile):
+    try:
+        comment = comment_models.Comment.objects.get(id=comment_id)
+        try:
+            like = comment_models.CommentLike.objects.get(
+                profile=profile,
+                comment=comment
+            )
+            like.delete()
+            return JsonResponse(
+                data={
+                    'ok': True,
+                    'description': 'Comment was unliked successfully'
+                }
+            )
+        except:
+            return JsonResponse(
+                data={
+                    'ok': False,
+                    'description': 'You unlike a comment you haven\'t liked yet!'
+                }
+            )
+    except:
+        return JsonResponse(
+            data={
+                'ok': False,
+                'description': 'Unable to unlike comment'
+            }
+        )
+
+
+@find_profile_decorator
+def submit_comment(request, commented_id, profile: espn_models.Profile):
+    try:
+        commented_type = request.GET['type']
+        data = json.loads(request.body)
+        comment = comment_models.Comment()
+        try:
+            comment_field = comment_models.CommentField.objects.get(
+                commented_id=commented_id,
+                field_type=commented_type,
+            )
+        except Exception:
+            return JsonResponse(
+                data={
+                    'ok': False,
+                    'description': 'Comment Field couldn\'t be found!'
+                }
+            )
+        else:
+            comment = comment_models.Comment()
+            comment.profile = profile
+            comment.field = comment_field
+            comment.text = data['text']
+            if not comment.text:
+                return JsonResponse(
+                    data={
+                        'ok': False,
+                        'description': 'Comment Text Shouldn\'t Be Empty!'
+                    }
+                )
+            try:
+                print(data['replyToID'])
+                comment.reply_to = comment_models.Comment.objects.get(id=data['replyToID'])
+            except:
+                pass
+
+            comment.save()
+            return JsonResponse(
+                data={
+                    'ok': True,
+                    'description': 'Comment was submitted successfully'
+                }
+            )
+    except:
+        return JsonResponse(
+            data={
+                'ok': False,
+                'description': 'Couldn\'t save your comment'
             }
         )
